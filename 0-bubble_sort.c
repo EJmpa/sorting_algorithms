d4d911feb3d78c8378f435067fbdad0b1b06754f
EJmpa <chukwuemeka.chukwu@eng.uniben.edu>
2023-04-23 10:57:00 -0700

    bubble

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,1 +1,18 @@
+#include "sort.h"
 
+/**
+ * bubble_sort - sort an array of integers using
+ * bubble sort method
+ * @array - an array to be sorted
+ * @size - size of the array
+ *
+ * Return None
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	for (int i=0; i<size-1; i++)
+	{
+
+	}
+}
