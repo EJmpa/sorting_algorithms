0165a58c6758b09d06dd5e1684693ef6d284961a
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-24 21:26:49 +0100

    Sort header file

--- sort.h
+++ sort.h
@@ -35,5 +35,9 @@ void radix_sort(int *array, size_t size);
 void bitonic_sort(int *array, size_t size);
 void quick_sort_hoare(int *array, size_t size);
 
+/* Comparison direction macros for bitonic sort */
+#define UP 0
+#define DOWN 1
+
 
 #endif /*SORT_H*/
