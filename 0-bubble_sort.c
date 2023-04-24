6e4eb1a48b190e2941864c5a5325d11b6bce10dd
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-24 09:37:24 +0100

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -24,21 +24,21 @@ void swap_ints(int *a, int *b)
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, len = size;
-	bool bubbly = false;
+	int flag = 0;
 
 	if (array == NULL || size < 2)
 		return;
 
-	while (bubbly == false)
+	while (flag == 0)
 	{
-		bubbly = true;
+		flag = 1;
 		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
 				swap_ints(array + i, array + i + 1);
 				print_array(array, size);
-				bubbly = false;
+				flag = 0;
 			}
 		}
 		len--;
