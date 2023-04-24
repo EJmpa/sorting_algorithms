3793f363a85923cfc527240dab61c7d835c30bf5
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-24 09:01:26 +0100

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -12,6 +12,7 @@
 void bubble_sort(int *array, size_t size)
 {
     size_t i, j;
+    int tmp;
 
 
     if (size<2 || array==NULL)
@@ -23,7 +24,6 @@ void bubble_sort(int *array, size_t size)
         int flag=0;
         for (j=0; j<size-1-i; j++)
         {
-            int tmp;
             if (array[j]>array[j+1])
             tmp=array[j];
             array[j]=array[j+1];
