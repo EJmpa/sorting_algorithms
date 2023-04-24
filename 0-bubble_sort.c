203aa40a14265dcdbae9760f3c05f207b02eb07b
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-24 09:33:12 +0100

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,40 +1,46 @@
 #include "sort.h"
 
 /**
- * bubble_sort - sort an array of integers using
- * bubble sort method
- * @array - an array to be sorted
- * @size - size of the array
- *
- * Return None
+ * swap_ints - Swap two integers in an array.
+ * @a: The first integer to swap.
+ * @b: The second integer to swap.
  */
+void swap_ints(int *a, int *b)
+{
+	int tmp;
 
+	tmp = *a;
+	*a = *b;
+	*b = tmp;
+}
+
+/**
+ * bubble_sort - Sort an array of integers in ascending order.
+ * @array: An array of integers to sort.
+ * @size: The size of the array.
+ *
+ * Description: Prints the array after each swap.
+ */
 void bubble_sort(int *array, size_t size)
 {
-    size_t i, j, len = size;
-    int tmp;
-    /*int flag;*/
+	size_t i, len = size;
+	bool bubbly = false;
 
+	if (array == NULL || size < 2)
+		return;
 
-    if (size<2 || array==NULL)
-    {
-        return;
-    }
-    for (i=0; i<len-1; i++)
-    {
-        /*flag=0;*/
-        for (j=0; j<len-1-i; j++)
-        {
-            if (array[j]>array[j+1])
-            tmp=array[j];
-            array[j]=array[j+1];
-            array[j+1]=tmp;
-            print_array(array, size);
-            /*flag=1;*/
-        }
-        /*if (flag==0)
-        {
-            break;
-        }*/
-    }
+	while (bubbly == false)
+	{
+		bubbly = true;
+		for (i = 0; i < len - 1; i++)
+		{
+			if (array[i] > array[i + 1])
+			{
+				swap_ints(array + i, array + i + 1);
+				print_array(array, size);
+				bubbly = false;
+			}
+		}
+		len--;
+	}
 }
