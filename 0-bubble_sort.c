9c4126bddf15511f5569d529cb54bb34b74d9093
Chukwu Chukwuemeka Jude <111859279+EJmpa@users.noreply.github.com>
2023-04-24 09:41:23 +0100

    Update

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -32,7 +32,7 @@ void bubble_sort(int *array, size_t size)
 	while (flag == 0)
 	{
 		flag = 1;
-		for (i = 0; i < len - 1 -i; i++)
+		for (i = 0; i < len - 1; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
